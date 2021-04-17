4f223868e3e963890b9e49f904bda1eb77ebd9c4
Jack Gordon <appjackstudio@gmail.com>
2021-04-17 02:06:13 -0500

    Load events from the Google Calender  API (WIP)

--- mental_health_calender/lib/cubit/calender_cubit.dart
+++ mental_health_calender/lib/cubit/calender_cubit.dart
@@ -2,14 +2,41 @@ import 'package:bloc/bloc.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:googleapis/calendar/v3.dart';
+import "package:http/http.dart";
 import 'package:meta/meta.dart';
 
 part 'calender_state.dart';
 
+class _AuthClient extends BaseClient {
+  final _client = Client();
+  final Map<String, String> Function() getAuthHeaders;
+
+  _AuthClient(this.getAuthHeaders);
+
+  @override
+  Future<StreamedResponse> send(BaseRequest request) {
+    request.headers.addAll(getAuthHeaders());
+    return _client.send(request);
+  }
+
+  @override
+  void close() {}
+}
+
 class GoogleCubit extends Cubit<GoogleState> {
-  Future<FirebaseApp> _firebaseInitialization = Firebase.initializeApp();
+  final Future<FirebaseApp> _firebaseInitialization = Firebase.initializeApp();
+  final googleSignIn = GoogleSignIn(scopes: [
+    CalendarApi.calendarReadonlyScope
+  ]);
+
+  Map<String, String> _headers;
+  _AuthClient _client;
+  CalendarApi _calendarApi;
 
   GoogleCubit() : super(GoogleUnauthenticated()) {
+    _client = _AuthClient(() => _headers);
+    _calendarApi = CalendarApi(_client);
     login();
   }
 
@@ -20,16 +47,18 @@ class GoogleCubit extends Cubit<GoogleState> {
       try {
         await _firebaseInitialization;
 
-        GoogleSignInAccount googleUser = await GoogleSignIn().signInSilently();
+        GoogleSignInAccount googleUser = await googleSignIn.signInSilently();
 
         if (googleUser == null) {
-          googleUser = await GoogleSignIn().signIn();
+          googleUser = await googleSignIn.signIn();
         }
 
         if (googleUser == null) {
           emit(GoogleUnauthenticated());
           return;
         }
+
+        _headers = await googleUser.authHeaders;
 
         // Obtain the auth details from the request
         final GoogleSignInAuthentication googleAuth =
@@ -47,9 +76,12 @@ class GoogleCubit extends Cubit<GoogleState> {
             await FirebaseAuth.instance.signInWithCredential(googleCredential);
 
         emit(GoogleAuthenticated(
+          googleUser: googleUser,
           googleCredential: googleCredential,
           firebaseCredential: firebaseCredential,
         ));
+
+        loadCalender();
       } catch (e) {
         emit(GoogleUnauthenticated(error: e));
       }
@@ -59,6 +91,18 @@ class GoogleCubit extends Cubit<GoogleState> {
   void loadCalender() async {
     if (state is GoogleAuthenticated && !(state is GoogleCalenderLoading)) {
       GoogleAuthenticated _state = state;
+      try {
+        emit(GoogleCalenderLoading(_state));
+
+        final calendars = await _calendarApi.calendarList.list();
+
+        final events = await Future.wait(calendars.items.map((calendar) => _calendarApi.events.list(calendar.id)));
+
+        emit(GoogleCalenderLoaded(_state));
+      } catch (e) {
+        print(e);
+        emit(GoogleAuthenticated.copy(_state));
+      }
     }
   }
 }
--- mental_health_calender/lib/cubit/calender_state.dart
+++ mental_health_calender/lib/cubit/calender_state.dart
@@ -14,15 +14,28 @@ class GoogleUnauthenticated extends GoogleState {
 class GoogleAuthenticated extends GoogleState {
   final GoogleAuthCredential googleCredential;
   final UserCredential firebaseCredential;
+  final GoogleSignInAccount googleUser;
 
   GoogleAuthenticated({
+    @required this.googleUser,
     @required this.googleCredential,
     @required this.firebaseCredential,
   });
+
+  GoogleAuthenticated.copy(GoogleAuthenticated auth)
+      : this(
+          googleUser: auth.googleUser,
+          googleCredential: auth.googleCredential,
+          firebaseCredential: auth.firebaseCredential,
+        );
 }
 
-class GoogleCalenderLoading extends GoogleAuthenticated {}
+class GoogleCalenderLoading extends GoogleAuthenticated {
+  GoogleCalenderLoading(GoogleAuthenticated auth) : super.copy(auth);
+}
 
 class GoogleCalenderLoaded extends GoogleAuthenticated {
   // TODO: Calender Data
+
+  GoogleCalenderLoaded(GoogleAuthenticated auth) : super.copy(auth);
 }
